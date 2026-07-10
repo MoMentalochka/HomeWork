1b1c19ace2d504f1c5246101340d904ee697534e
Franki Alladay <franki.alladay@onlinesup.com>
2026-07-10 19:35:34 +0300

    update task file mockery + tests tasks

--- inventory/internal/repository/inventory/repository.go
+++ inventory/internal/repository/inventory/repository.go
@@ -17,8 +17,8 @@ func NewRepository() *repository {
 		data: make(map[string]repomodel.Part),
 	}
 
-	repo.data["1"] = repomodel.Part{Uuid: "1", Price: 10.0}
-	repo.data["2"] = repomodel.Part{Uuid: "2", Price: 13.0}
+	repo.data["1"] = repomodel.Part{Uuid: "1", Price: 10.0, Name: "Product 1"}
+	repo.data["2"] = repomodel.Part{Uuid: "2", Price: 13.0, Name: "Product 2"}
 
 	return &repo
 }
