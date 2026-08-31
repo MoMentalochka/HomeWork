7c0ca8b14a19184e51ef53d4010d8aacae4f0d10
mental-pro <peacefull.lands@gmail.com>
2026-08-31 14:41:48 +0300

    fix build issues

--- inventory/internal/repository/inventory/repository.go
+++ inventory/internal/repository/inventory/repository.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"log"
 
+	// inventoryv1 "github.com/MoMentalochka/HomeWork/shared/pkg/proto/inventory/v1"
+
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 	"go.mongodb.org/mongo-driver/v2/mongo/options"
@@ -37,34 +39,34 @@ func NewRepository(ctx context.Context, client *mongo.Client) *repository {
 	// partUUID := gofakeit.UUID()
 	// now := time.Now()
 
-	//	partDoc := bson.M{
-	//	"_id":            partUUID,
-	//	"name":           gofakeit.ProductName(),
-	//	"description":    gofakeit.Sentence(15),
-	//	"price":          gofakeit.Float64Range(1000, 500000),
-	//	"stock_quantity": int64(gofakeit.Number(1, 100)),
-	//	"category":       inventoryv1.Category_CATEGORY_WING.String(),
-	//	"dimensions": bson.M{
-	//		"length": gofakeit.Float64Range(10, 500),
-	//		"width":  gofakeit.Float64Range(10, 200),
-	//		"height": gofakeit.Float64Range(5, 100),
-	//		"weight": gofakeit.Float64Range(1, 1000),
-	//	},
-	//	"manufacturer": bson.M{
-	//		"name":    gofakeit.Company(),
-	//		"country": gofakeit.Country(),
-	//		"website": "https://" + gofakeit.DomainName(),
-	//	},
-	//	"tags":       []string{gofakeit.Word(), gofakeit.Word(), gofakeit.Word()},
-	//	"created_at": bson.NewDateTimeFromTime(now),
-	//	"updated_at": bson.NewDateTimeFromTime(now),
-	//}
-	//
-	//	_, err = repo.collection.InsertOne(ctx, partDoc)
-	//	if err != nil {
-	//	log.Printf("Ошибка вставки заметки: %v\n", err)
-	//	return &repo
-	//	}
+	// partDoc := bson.M{
+	// 	"_id":            partUUID,
+	// 	"name":           gofakeit.ProductName(),
+	// 	"description":    gofakeit.Sentence(15),
+	// 	"price":          gofakeit.Float64Range(1000, 500000),
+	// 	"stock_quantity": int64(gofakeit.Number(1, 100)),
+	// 	"category":       inventoryv1.Category_CATEGORY_WING.String(),
+	// 	"dimensions": bson.M{
+	// 		"length": gofakeit.Float64Range(10, 500),
+	// 		"width":  gofakeit.Float64Range(10, 200),
+	// 		"height": gofakeit.Float64Range(5, 100),
+	// 		"weight": gofakeit.Float64Range(1, 1000),
+	// 	},
+	// 	"manufacturer": bson.M{
+	// 		"name":    gofakeit.Company(),
+	// 		"country": gofakeit.Country(),
+	// 		"website": "https://" + gofakeit.DomainName(),
+	// 	},
+	// 	"tags":       []string{gofakeit.Word(), gofakeit.Word(), gofakeit.Word()},
+	// 	"created_at": bson.NewDateTimeFromTime(now),
+	// 	"updated_at": bson.NewDateTimeFromTime(now),
+	// }
+
+	// _, err = repo.collection.InsertOne(ctx, partDoc)
+	// if err != nil {
+	// 	log.Printf("Ошибка вставки заметки: %v\n", err)
+	// 	return &repo
+	// }
 
 	return &repo
 }
